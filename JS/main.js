79c610e853757bdb4e8afc65ef66480b9cd290ea
“codewriterX” <miturugi.infinite@gmail.com>
2023-03-01 20:03:23 +0900

    2023-3-01

---
+++ JS/main.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper(".swiper", {
+  slidesPerView: 4/* この行を追加 */,
+  // ページネーションが必要なら追加
+  pagination: {
+    el: ".swiper-pagination"
+  },
+
+  // ナビボタンが必要なら追加
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  }
+});

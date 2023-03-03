3c2bb6cf168dee1afc1d8ca377e1b5040474a347
codewriterX <miturugi.infinite@gmail.com>
2023-03-03 11:52:23 +0900

    2023-3-03

--- JS/main.js
+++ JS/main.js
@@ -1,13 +1,30 @@
-const swiper = new Swiper(".swiper", {
-  slidesPerView: 4/* この行を追加 */,
-  // ページネーションが必要なら追加
-  pagination: {
-    el: ".swiper-pagination"
-  },
 
-  // ナビボタンが必要なら追加
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev"
-  }
+
+if (window.matchMedia('(max-width: 768px)').matches) {
+  const swiper = new Swiper(".swiper", {
+    slidesPerView: 1/* この行を追加 */,
+    // ページネーションが必要なら追加
+    pagination: {
+      el: ".swiper-pagination"
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    }
 });
+} 
+else if (window.matchMedia('(min-width:768px)').matches) {
+  const swiper = new Swiper(".swiper", {
+    slidesPerView: 4/* この行を追加 */,
+    // ページネーションが必要なら追加
+    pagination: {
+      el: ".swiper-pagination"
+    },
+  
+    // ナビボタンが必要なら追加
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    }
+  });
+}
